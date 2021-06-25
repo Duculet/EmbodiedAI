f4a25c01fe82b1fef03a8f46c25241f997a1b5f0
Duculet <ducu@programmer.net>
2021-06-25 12:05:20 +0300

    QuickFix

--- main.py
+++ main.py
@@ -21,30 +21,40 @@ base_path = ""
 if __name__ == "__main__":
     start = time.time()
 
-    for mask, lockdown, p_vax in parameters:
-        path = f'experiments/covid/tests/{mask}-{lockdown}-{p_vax}/'
-        config["screen"]["current_path"] = path
-        if not os.path.exists(path):
-            os.mkdir(path)
-        with open(path + "results.csv", 'w', encoding="utf-8", newline='') as f:
-            # create the csv writer
-            writer = csv.writer(f)
-            # write a row to the csv file
-            writer.writerow(["Mask", "Lockdown", "P_Vax"])
-            writer.writerow([mask, lockdown, p_vax])
+    if config["screen"]["testing"]:
+        for mask, lockdown, p_vax in parameters:
+            path = f'experiments/covid/tests/{mask}-{lockdown}-{p_vax}/'
+            config["screen"]["current_path"] = path
+            if not os.path.exists(path):
+                os.mkdir(path)
+            with open(path + "results.csv", 'w', encoding="utf-8", newline='') as f:
+                # create the csv writer
+                writer = csv.writer(f)
+                # write a row to the csv file
+                writer.writerow(["Mask", "Lockdown", "P_Vax"])
+                writer.writerow([mask, lockdown, p_vax])
 
-        config["environment"]["mask"] = mask
-        config["environment"]["lockdown"] = lockdown
-        config["environment"]["p_vaccination"] = p_vax
-        for i in range(1, config["screen"]["runs"] + 1):
-            config["screen"]["current_run"] = i
-            pygame.init()
-            sim = Simulation(
-                num_agents=config["base"]["n_agents"],
-                screen_size=(config["screen"]["width"],
-                             config["screen"]["height"]),
-                swarm_type=config["base"]["swarm_type"],
-                iterations=config["screen"]["frames"])
-            sim.run()
+            config["environment"]["mask"] = mask
+            config["environment"]["lockdown"] = lockdown
+            config["environment"]["p_vaccination"] = p_vax
+            for i in range(1, config["screen"]["runs"] + 1):
+                config["screen"]["current_run"] = i
+                pygame.init()
+                sim = Simulation(
+                    num_agents=config["base"]["n_agents"],
+                    screen_size=(config["screen"]["width"],
+                                 config["screen"]["height"]),
+                    swarm_type=config["base"]["swarm_type"],
+                    iterations=config["screen"]["frames"])
+                sim.run()
+    else:
+        pygame.init()
+        sim = Simulation(
+            num_agents=config["base"]["n_agents"],
+            screen_size=(config["screen"]["width"],
+                         config["screen"]["height"]),
+            swarm_type=config["base"]["swarm_type"],
+            iterations=config["screen"]["frames"])
+        sim.run()
     end = time.time()
     print("Total runtime:", end - start)
